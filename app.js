c90db399da2bcfe675860d38e7dc988e42acb6be
Muryllo Vieira <muryllovieira59@gmail.com>
2023-03-23 16:04:36 -0300

    finalizado

--- app.js
+++ app.js
@@ -17,9 +17,13 @@ const getEstados = async(event) => {
     document.getElementById('estado').textContent = estado.descricao
     document.getElementById('capital').textContent = estado.capital
     document.getElementById('regiao').textContent =  estado.regiao
+    // const div = document.getElementById('div-lista')
     
+    // div.textContent = ''
     const ul = document.getElementById('lista-cidades')
-
+    
+    // div.appendChild(ul)
+    ul.textContent = '';
     cidades.cidades.forEach(function(cidade){
         const lista = document.createElement('li')
         lista.textContent = cidade
